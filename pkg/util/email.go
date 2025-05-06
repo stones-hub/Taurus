ab79f9a146bd3456a9f57287c03d039d8ed8fc44
STones_ <61647649@qq.com>
2025-05-06 10:36:47 +0800

    add

--- pkg/util/email.go
+++ pkg/util/email.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"Taurus/pkg/loggerx"
 	"strings"
 	"time"
 
@@ -8,13 +9,15 @@ import (
 )
 
 type EmailInfo struct {
-	Username       string        // 61647649@qq.com
-	Password       string        // 邮箱授权码
-	ConnectTimeout time.Duration // 链接超时时间
-	SendTimeout    time.Duration // 发送邮件超时时间
-	Host           string        // smtp.example.com ， 邮件服务器地址
-	Port           int           // 端口
-	KeepAlive      bool          // 是否长链
+	Username       string          // 61647649@qq.com
+	Password       string          // 邮箱授权码
+	ConnectTimeout time.Duration   // 链接超时时间
+	SendTimeout    time.Duration   // 发送邮件超时时间
+	Host           string          // smtp.example.com ， 邮件服务器地址
+	Port           int             // 端口
+	KeepAlive      bool            // 是否长链
+	Encryption     mail.Encryption // 加密方式 encryption: mail.EncryptionNone, mail.EncryptionSSLTLS, mail.EncryptionSTARTTLS, 其他的已经废弃
+	Auth           mail.AuthType   // 认证方式 mail.AuthCRAMMD5, mail.AuthLogin, mail.AuthPlain
 }
 
 // Content 邮件内容
@@ -59,117 +62,111 @@ func send(content *EmailContent, to string, smtpClient *mail.SMTPClient) error {
 	return email.Send(smtpClient)
 }
 
-func SendMail(content *EmailContent, to string, info EmailInfo) (err error) {
-	var (
-		emailClient = mail.NewSMTPClient()
-		smtpClient  *mail.SMTPClient
-	)
-
-	emailClient.Host = info.Host
-	emailClient.Port = info.Port
-	emailClient.Username = info.Username
-	emailClient.Password = info.Password
-	emailClient.Encryption = mail.EncryptionNone
-	emailClient.ConnectTimeout = info.ConnectTimeout
-	emailClient.SendTimeout = info.SendTimeout
-	emailClient.KeepAlive = info.KeepAlive
-
-	smtpClient, err = emailClient.Connect()
-	if err != nil {
-		return
-	}
-
-	//NOOP command, optional, usedfor avoid timeout when KeepAlive is true and you aren't sending mails.
-	//Execute this command each 30 seconds is ideal for persistent connection
-	err = smtpClient.Noop()
-	if err != nil {
-		return
-	}
-
-	err = send(content, to, smtpClient)
-	return
+// configureSMTPClient configure the SMTP client
+// encryption: mail.EncryptionNone, mail.EncryptionSSLTLS, mail.EncryptionSTARTTLS, 其他的已经废弃
+// 高加密方式,使用mail.EncryptionSSLTLS，低加密方式，使用mail.EncryptionSTARTTLS
+// 安全性：AuthCRAMMD5 > AuthLogin > AuthPlain
+// 兼容性：AuthPlain和AuthLogin通常比AuthCRAMMD5更广泛支持。
+// 使用建议：在加密连接（如TLS或SSL）上使用AuthPlain或AuthLogin，以确保凭据的安全性。如果服务器支持并且需要更高的安全性，可以选择AuthCRAMMD5。
+func configureSMTPClient(info EmailInfo) (*mail.SMTPClient, error) {
+	smtpServer := mail.NewSMTPClient()
+	smtpServer.Host = info.Host
+	smtpServer.Port = info.Port
+	smtpServer.Username = info.Username
+	smtpServer.Password = info.Password
+	smtpServer.Encryption = info.Encryption
+	smtpServer.ConnectTimeout = info.ConnectTimeout
+	smtpServer.SendTimeout = info.SendTimeout
+	smtpServer.KeepAlive = info.KeepAlive
+	smtpServer.Authentication = info.Auth
+	return smtpServer.Connect()
 }
 
-func SendMultipleEmails(content *EmailContent, toList []string, info EmailInfo) (err error) {
-
-	var (
-		emailClient = mail.NewSMTPClient()
-		smtpClient  *mail.SMTPClient
-	)
-	emailClient.Host = info.Host
-	emailClient.Port = info.Port
-	emailClient.Username = info.Username
-	emailClient.Password = info.Password
-	emailClient.Encryption = mail.EncryptionNone
-	emailClient.ConnectTimeout = info.ConnectTimeout
-	emailClient.SendTimeout = info.SendTimeout
-
-	//KeepAlive true because the connection need to be open for multiple emails
-	//For avoid inactivity timeout, every 30 second you can send a NO OPERATION command to smtp client
-	//use smtpClient.Client.Noop() after 30 second of inactivity in this example
-	emailClient.KeepAlive = true
-
-	//For authentication you can use AuthPlain, AuthLogin or AuthCRAMMD5
-	emailClient.Authentication = mail.AuthPlain
-
-	smtpClient, err = emailClient.Connect()
-
+// SendMail with one email, keepAlive is false
+func SendMail(content *EmailContent, to string, info EmailInfo) error {
+	smtpClient, err := configureSMTPClient(info)
 	if err != nil {
-		return
+		return err
 	}
+	defer smtpClient.Close()
 
-	// toList := [3]string{"to1@example1.com", "to3@example2.com", "to4@example3.com"}
+	if err = send(content, to, smtpClient); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SendMultipleEmails with multiple emails, keepAlive is true and Noop every 30 seconds
+func SendMultipleEmails(content *EmailContent, toList []string, info EmailInfo) error {
+	smtpClient, err := configureSMTPClient(info)
+	if err != nil {
+		return err
+	}
+	defer smtpClient.Close()
+
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	done := make(chan bool)
+
+	// start a goroutine to send NOOP command every 10 seconds
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				err := smtpClient.Noop()
+				if err != nil {
+					loggerx.DefaultLogger.Error("Failed to send NOOP: %v", err)
+					return
+				}
+			}
+		}
+	}()
+
+	// 发送邮件
 	for _, to := range toList {
 		err = send(content, to, smtpClient)
 		if err != nil {
-			return
+			loggerx.DefaultLogger.Error("Failed to send email to %s: %v", to, err)
+			done <- true
+			return err
 		}
 	}
-	return
+
+	// 发送完成后停止NOOP goroutine
+	done <- true
+	return nil
 }
 
-func SendMailWithTSL(content *EmailContent, to string, info EmailInfo) (err error) {
-	var (
-		emailClient = mail.NewSMTPClient()
-		smtpClient  *mail.SMTPClient
-	)
-	emailClient.Host = info.Host
-	emailClient.Port = info.Port
-	emailClient.Username = info.Username
-	emailClient.Password = info.Password
-	emailClient.Encryption = mail.EncryptionSTARTTLS
-	emailClient.ConnectTimeout = info.ConnectTimeout
-	emailClient.SendTimeout = info.SendTimeout
-	emailClient.KeepAlive = info.KeepAlive
-
-	smtpClient, err = emailClient.Connect()
+// SendWithTLS 使用TLS加密发送邮件 安全性：AuthCRAMMD5 > AuthLogin > AuthPlain
+func SendWithTLS(content *EmailContent, to string, info EmailInfo) error {
+	info.Encryption = mail.EncryptionSTARTTLS
+	info.Auth = mail.AuthLogin
+	smtpClient, err := configureSMTPClient(info)
 	if err != nil {
-		return
+		return err
 	}
+	defer smtpClient.Close()
 
-	err = send(content, to, smtpClient)
-	return
+	if err = send(content, to, smtpClient); err != nil {
+		return err
+	}
+	return nil
 }
 
-func SendMailWithSSL(content *EmailContent, to string, info EmailInfo) (err error) {
-	var (
-		emailClient = mail.NewSMTPClient()
-		smtpClient  *mail.SMTPClient
-	)
-	emailClient.Host = info.Host
-	emailClient.Port = info.Port
-	emailClient.Username = info.Username
-	emailClient.Password = info.Password
-	emailClient.Encryption = mail.EncryptionSSLTLS
-	emailClient.ConnectTimeout = info.ConnectTimeout
-	emailClient.SendTimeout = info.SendTimeout
-	emailClient.KeepAlive = info.KeepAlive
-
-	smtpClient, err = emailClient.Connect()
+// SendWithSSL 使用SSL加密发送邮件 安全性：AuthCRAMMD5 > AuthLogin > AuthPlain
+func SendWithSSL(content *EmailContent, to string, info EmailInfo) error {
+	info.Encryption = mail.EncryptionSSLTLS
+	info.Auth = mail.AuthLogin
+	smtpClient, err := configureSMTPClient(info)
 	if err != nil {
-		return
+		return err
 	}
+	defer smtpClient.Close()
 
-	err = send(content, to, smtpClient)
-	return
+	if err = send(content, to, smtpClient); err != nil {
+		return err
+	}
+	return nil
 }
