6c9b7ec8060ff8496fa361c51b00f2d2e7a4c201
STones_ <61647649@qq.com>
2025-05-17 13:39:33 +0800

    update mcp resource

--- internal/app/core/mcps/prompts/prompt_demo.go
+++ internal/app/core/mcps/prompts/prompt_demo.go
@@ -1,5 +1,43 @@
 package prompts
 
-func init() {
+import (
+	"Taurus/pkg/logx"
+	"Taurus/pkg/mcp"
+	"context"
 
+	"github.com/ThinkInAIXYZ/go-mcp/protocol"
+)
+
+func init() {
+	mcp.MCPHandler.RegisterPrompt(prompt(), promptHandler)
+}
+
+func prompt() *protocol.Prompt {
+	return &protocol.Prompt{
+		Name:        "system_prompt",
+		Description: "system prompt",
+		Arguments: []protocol.PromptArgument{
+			{
+				Name:        "system_prompt_argument",
+				Description: "system prompt argument description",
+				Required:    true,
+			},
+		},
+	}
+}
+
+func promptHandler(ctx context.Context, request *protocol.GetPromptRequest) (*protocol.GetPromptResult, error) {
+	logx.Core.Info("default", "call prompt, request: %v", request)
+	return &protocol.GetPromptResult{
+		Messages: []protocol.PromptMessage{
+			{
+				Role: protocol.RoleUser,
+				Content: &protocol.TextContent{
+					Type: "text",
+					Text: "Prompt handler content",
+				},
+			},
+		},
+		Description: "Prompt handler description",
+	}, nil
 }
