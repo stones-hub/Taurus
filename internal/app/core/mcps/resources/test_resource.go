f8d253803082730384c61167dbbcc61eda475013
STones_ <61647649@qq.com>
2025-05-17 13:20:14 +0800

    update mcp resource

--- internal/app/core/mcps/resources/test_resource.go
+++ internal/app/core/mcps/resources/test_resource.go
@@ -9,29 +9,23 @@ import (
 )
 
 func init() {
-	Do()
-}
-
-func Do() {
 	// add resource
-	testResource := &protocol.Resource{
+	mcp.MCPHandler.RegisterResource(&protocol.Resource{
 		URI:      "file:///test.txt",
 		Name:     "test.txt",
 		MimeType: "text/plain-txt",
-	}
-	testResourceContent := protocol.TextResourceContents{
-		URI:      testResource.URI,
-		MimeType: testResource.MimeType,
-		Text:     "test",
-	}
+	}, TestResource)
+}
 
-	mcp.MCPHandler.RegisterResource(testResource, func(ctx context.Context, request *protocol.ReadResourceRequest) (*protocol.ReadResourceResult, error) {
-		logx.Core.Info("default", "read resource, request: %v", request)
-		return &protocol.ReadResourceResult{
-			Contents: []protocol.ResourceContents{
-				testResourceContent,
+func TestResource(ctx context.Context, request *protocol.ReadResourceRequest) (*protocol.ReadResourceResult, error) {
+	logx.Core.Info("default", "read resource, request: %v", request)
+	return &protocol.ReadResourceResult{
+		Contents: []protocol.ResourceContents{
+			protocol.TextResourceContents{
+				URI:      "file:///test.txt",
+				MimeType: "text/plain-txt",
+				Text:     "test",
 			},
-		}, nil
-	})
-
+		},
+	}, nil
 }
